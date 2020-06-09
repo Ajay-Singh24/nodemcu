ed293e0e097d66776aed63840420b4f18308f521
singha024 <62828638+singha024@users.noreply.github.com>
2020-06-09 05:54:09 +0100

    Add files via upload

---
+++ analof_input_ldr/analof_input_ldr.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(D2,OUTPUT);
+}
+
+void loop() {
+  int s = analogRead(A0);
+  Serial.println(s);
+  if (s<5){
+    digitalWrite(D2,HIGH);
+    }
+  else{
+    digitalWrite(D2,LOW);
+    }  
+  delay(500);
+}
